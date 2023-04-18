d379d89c6efb1da89c3279e4354690850b88c18b
jiaruling@libii.com <jiaruling@libii.com>
2023-04-18 18:10:49 +0800

    ✨ feat(all): 目录结构优化

---
+++ initial/initial.go
@@ -0,0 +1,6 @@
+package initial
+
+func init() {
+	// 加载配置文件
+	// 初始化数据库连接
+}
--- main.go
+++ main.go
@@ -5,15 +5,17 @@ import (
 	"os/signal"
 	"syscall"
 
+	_ "github.com/jiaruling/Gateway/initial"
 	"github.com/jiaruling/Gateway/router"
 )
 
 func main() {
+	// 启动服务
 	router.HttpServerRun()
-
+	// 优雅退出
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-
+	// 销毁服务
 	router.HttpServerStop()
 }
